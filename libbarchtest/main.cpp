f73c7d0e487b02b3fe64a3b2289b730b2084d88f
AlexTyrsa <alextyrsa@gmail.com>
2024-04-19 07:39:08 +0300

    path from cmdline, default path is Documents

--- libbarchtest/main.cpp
+++ libbarchtest/main.cpp
@@ -1,6 +1,8 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQuickItem>
+#include <QStandardPaths>
+#include <QDir>
 
 #include "QFileListData.h"
 #include "QFileListDataItemI.h"
@@ -29,7 +31,19 @@ int main(int argc, char *argv[])
     QObject* viewObj = rootObj->findChild<QQuickItem*>("fileView");
 
     if(viewObj)
-        viewObj->setProperty("dataList", QVariant::fromValue(new QFileListData("/Volumes/Transcend/")));
+    {
+        QString defaultPath = QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation);
+
+        if(QCoreApplication::arguments().length() > 1)
+        {
+            QString argPath = QCoreApplication::arguments().at(1);
+
+            if(QDir(argPath).exists())
+                defaultPath = argPath;
+        }
+
+        viewObj->setProperty("dataList", QVariant::fromValue(new QFileListData(defaultPath)));
+    }
 
     return app.exec();
 }
